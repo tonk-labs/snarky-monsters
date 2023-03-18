3e4a1a5deac407643dc3e1263ed910c61f1bf8d8
Harry Kingdon <harrykingdon@Harrys-MacBook-Pro.local>
2023-03-18 17:59:31 +0000

    phaser boilerplate

--- client/src/pages/_document.js
+++ client/src/pages/_document.js
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Snarky Monsters!</title>
+        <meta
+          name="description"
+          content="Battle your way to web3 domination against ZK-powered NPCs"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -27,53 +27,42 @@ const About = styled.div``
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Snarky Monsters!</title>
-        <meta
-          name="description"
-          content="Battle your way to web3 domination against ZK-powered NPCs"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
-      <Container>
-        <Leaderboard />
-        <Preview className="bigBox">
-          <h2>Here's a nice preview of the game</h2>
-          <Link href="/game">
-            <button>Play game</button>
-          </Link>
-        </Preview>
-        <About className="bigBox">
-          <h2>Welcome to snarky monsters</h2>
-          <p>
-            Get ready to dive into the world of Web3 and battle it out for
-            ultimate dominance in this thrilling new video game! As a player,
-            you'll face off against some of the toughest opponents the Web3 has
-            to offer, using your wits, strategy, and a little bit of luck to
-            emerge victorious.
-          </p>
-          <p>
-            With each NPC opponent you face, you'll need to carefully study
-            their play patterns and learn their strengths and weaknesses in
-            order to defeat them. But don't get too comfortable - the Web3 is
-            constantly evolving, and you'll need to stay on your toes if you
-            want to come out on top. Whether you prefer to use brute force or
-            cunning tactics, this game has something for everyone.{' '}
-          </p>
-          <hr />
-          <h3>What is this?</h3>
-          <p>
-            Accessor Labs’ ZK-ML tooling empowers game creators to enrich their
-            games with vibrant, trustless, intelligent agents. Furthermore, we
-            are developing our own games to test this capability and inspire a
-            community of game devs. Right now we are working on a cooking game
-            that showcases proof of LSTM model execution and are excited to
-            share it out in Q1 2023.{' '}
-          </p>
-        </About>
-      </Container>
-    </>
+    <Container>
+      <Leaderboard />
+      <Preview className="bigBox">
+        <h2>Here's a nice preview of the game</h2>
+        <Link href="/game">
+          <button>Play game</button>
+        </Link>
+      </Preview>
+      <About className="bigBox">
+        <h2>Welcome to snarky monsters</h2>
+        <p>
+          Get ready to dive into the world of Web3 and battle it out for
+          ultimate dominance in this thrilling new video game! As a player,
+          you'll face off against some of the toughest opponents the Web3 has to
+          offer, using your wits, strategy, and a little bit of luck to emerge
+          victorious.
+        </p>
+        <p>
+          With each NPC opponent you face, you'll need to carefully study their
+          play patterns and learn their strengths and weaknesses in order to
+          defeat them. But don't get too comfortable - the Web3 is constantly
+          evolving, and you'll need to stay on your toes if you want to come out
+          on top. Whether you prefer to use brute force or cunning tactics, this
+          game has something for everyone.{' '}
+        </p>
+        <hr />
+        <h3>What is this?</h3>
+        <p>
+          Accessor Labs’ ZK-ML tooling empowers game creators to enrich their
+          games with vibrant, trustless, intelligent agents. Furthermore, we are
+          developing our own games to test this capability and inspire a
+          community of game devs. Right now we are working on a cooking game
+          that showcases proof of LSTM model execution and are excited to share
+          it out in Q1 2023.{' '}
+        </p>
+      </About>
+    </Container>
   )
 }
